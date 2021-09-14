6fdd4cb09fd23c3e00d8f7c616c2db0911db743f
Matheus Pereira Gonçalves <32481629+matheusPereiraG@users.noreply.github.com>
2021-09-14 16:18:55 +0100

    #3.7 feat: Sleeping schedule logic correction

--- PracticeSet2/app/src/main/java/com/example/android/practiceset2/MainActivity.java
+++ PracticeSet2/app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.practiceset2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        int weekday = 5;
+        int weekend = 9;
+        int optimalHours = 7 * 8;
+
+        int actualHours = weekday * 5;
+        actualHours = actualHours + weekend * 2;
+        int solution = optimalHours - actualHours;
+        display(solution);
+    }
+
+    public void display (int i) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(""+i);
     }
 }
