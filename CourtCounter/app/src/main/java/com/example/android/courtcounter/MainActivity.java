2833c23ab2c80b495afd786e2490018f06ec7b61
Matheus Pereira Gonçalves <32481629+matheusPereiraG@users.noreply.github.com>
2021-09-15 09:54:00 +0100

    #3.27 feat: Apply weights on layouts

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,33 +7,53 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView pointsView;
-    private int totalPoints;
+    private TextView pointsViewA;
+    private TextView pointsViewB;
+    private int totalPointsA;
+    private int totalPointsB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pointsView = (TextView) findViewById(R.id.text_score_view);
-        totalPoints = Integer.parseInt(pointsView.getText().toString());
+        pointsViewA = (TextView) findViewById(R.id.text_scoreA_view);
+        pointsViewB = (TextView) findViewById(R.id.text_scoreB_view);
+        totalPointsA = Integer.parseInt(pointsViewA.getText().toString());
+        totalPointsB = Integer.parseInt(pointsViewB.getText().toString());
     }
 
-    public void pointer3(View view) {
-        totalPoints = totalPoints + 3;
-        display();
+    public void pointer3A(View view) {
+        totalPointsA = totalPointsA + 3;
+        displayForTeamA();
     }
 
-    public void pointer2(View view) {
-        totalPoints = totalPoints + 2;
-        display();
+    public void pointer2A(View view) {
+        totalPointsA = totalPointsA + 2;
+        displayForTeamA();
     }
 
-    public void pointer1(View view) {
-        totalPoints = totalPoints + 1;
-        display();
+    public void pointer1A(View view) {
+        totalPointsA = totalPointsA + 1;
+        displayForTeamA();
     }
 
-    private void display(){
-        pointsView.setText("" +totalPoints);
+    public void pointer3B(View view) {
+        totalPointsB = totalPointsB + 3;
+        displayForTeamB();
     }
+
+    public void pointer2B(View view) {
+        totalPointsB = totalPointsB + 2;
+        displayForTeamB();
+    }
+
+    public void pointer1B(View view) {
+        totalPointsB = totalPointsB + 1;
+        displayForTeamB();
+    }
+
+    private void displayForTeamA(){
+        pointsViewA.setText(String.valueOf(totalPointsA));
+    }
+    private void displayForTeamB() { pointsViewB.setText(String.valueOf(totalPointsB));}
 }
