eb1bae4640eb5686725fc299ce5c68b715d3fa26
Matheus Pereira Gonçalves <32481629+matheusPereiraG@users.noreply.github.com>
2021-09-20 15:17:52 +0100

    S04.01 feat: Add back button

---
+++ Module2/Sunshine/S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -0,0 +1,36 @@
+package com.example.android.sunshine;
+
+import android.content.Intent;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ActionBar back = getSupportActionBar();
+        try {
+            assert back != null;
+            back.setDisplayHomeAsUpEnabled(true);
+        }
+        catch (NullPointerException e){
+            Log.e(this.getClass().getSimpleName(), "Null pointer!!");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
