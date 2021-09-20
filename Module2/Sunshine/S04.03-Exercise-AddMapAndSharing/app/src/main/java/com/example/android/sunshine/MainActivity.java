1fcc9e0a3b751e4dea0fe731eed49ce248c3e7e2
Matheus Pereira Gonçalves <32481629+matheusPereiraG@users.noreply.github.com>
2021-09-20 15:57:40 +0100

    S04.03 feat: Add share and open maps

--- Module2/Sunshine/S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ Module2/Sunshine/S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -20,6 +20,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -221,8 +222,34 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
             return true;
         }
 
-        // TODO (2) Launch the map when the map menu item is clicked
+        if (id == R.id.open_map) {
+            openMap();
+            return true;
+        }
+
+        if(id == R.id.share) {
+            shareWeather();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void shareWeather() {
+        ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText("Hello!")
+                .startChooser();
+    }
+
+    private void openMap() {
+        String address = "Ponte de Lima";
+        Uri geoLocation = Uri.parse("geo:0,0?q=" + address);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
