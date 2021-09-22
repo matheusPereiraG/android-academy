efacdccb8aee8ea116783d799388149522020e09
Matheus Pereira Gonçalves <32481629+matheusPereiraG@users.noreply.github.com>
2021-09-22 11:28:56 +0100

    T06.09 feat: Add EditTextPreference to settings

--- Module2/Exercises/Lesson06-Visualizer-Preferences/T06.09-Exercise-EditTextPreference/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ Module2/Exercises/Lesson06-Visualizer-Preferences/T06.09-Exercise-EditTextPreference/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -61,8 +61,15 @@ public class VisualizerActivity extends AppCompatActivity implements SharedPrefe
                 getResources().getBoolean(R.bool.pref_show_treble_default)));
         mVisualizerView.setMinSizeScale(1);
         loadColorFromPreferences(sharedPreferences);
+        loadSizeFromPreferences(sharedPreferences);
         // Register the listener
         sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    private void loadSizeFromPreferences(SharedPreferences sharedPreferences) {
+        String value = sharedPreferences.getString(getString(R.string.pref_size_key)
+                ,getString(R.string.pref_size_default));
+        mVisualizerView.setMinSizeScale(Float.parseFloat(value));
     }
 
     private void loadColorFromPreferences(SharedPreferences sharedPreferences) {
@@ -82,6 +89,9 @@ public class VisualizerActivity extends AppCompatActivity implements SharedPrefe
             mVisualizerView.setShowTreble(sharedPreferences.getBoolean(key, getResources().getBoolean(R.bool.pref_show_treble_default)));
         } else if (key.equals(getString(R.string.pref_color_key))) {
             loadColorFromPreferences(sharedPreferences);
+        }
+        else if (key.equals(getString(R.string.pref_size_key))){
+            loadSizeFromPreferences(sharedPreferences);
         }
     }
 
