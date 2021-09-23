a95067108057317f44ea39f1947a1fdd7c3f2a44
Matheus Pereira Gonçalves <32481629+matheusPereiraG@users.noreply.github.com>
2021-09-23 15:58:37 +0100

    T09b.10 feat: Add view model and view model factory

--- Module2/Exercises/Lesson09b-ToDo-List-AAC/T09b.10-Exercise-AddViewModelToAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskViewModelFactory.java
+++ Module2/Exercises/Lesson09b-ToDo-List-AAC/T09b.10-Exercise-AddViewModelToAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskViewModelFactory.java
@@ -1,17 +1,24 @@
 package com.example.android.todolist;
 
-// TODO (1) Make this class extend ViewModel ViewModelProvider.NewInstanceFactory
-public class AddTaskViewModelFactory {
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.content.Context;
 
-    // TODO (2) Add two member variables. One for the database and one for the taskId
+import com.example.android.todolist.database.AppDatabase;
 
-    // TODO (3) Initialize the member variables in the constructor with the parameters received
+public class AddTaskViewModelFactory extends ViewModelProvider.NewInstanceFactory {
 
-    // TODO (4) Uncomment the following method
-    // Note: This can be reused with minor modifications
-    /*@Override
+    private int mTaskId;
+    private AppDatabase mDb;
+
+    AddTaskViewModelFactory(int taskId, AppDatabase db){
+        this.mTaskId = taskId;
+        mDb = db;
+    }
+
+    @Override
     public <T extends ViewModel> T create(Class<T> modelClass) {
         //noinspection unchecked
         return (T) new AddTaskViewModel(mDb, mTaskId);
-    }*/
+    }
 }
