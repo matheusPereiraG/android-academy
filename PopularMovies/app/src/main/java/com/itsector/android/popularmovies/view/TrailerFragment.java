1e9fe8aa456db790376c6b2139e0298ff2210829
Matheus Pereira Gonçalves <32481629+matheusPereiraG@users.noreply.github.com>
2021-10-06 18:47:43 +0100

    Launch intent for youtube videos

--- PopularMovies/app/src/main/java/com/itsector/android/popularmovies/view/TrailerFragment.java
+++ PopularMovies/app/src/main/java/com/itsector/android/popularmovies/view/TrailerFragment.java
@@ -1,41 +1,33 @@
 package com.itsector.android.popularmovies.view;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.itsector.android.popularmovies.R;
 import com.itsector.android.popularmovies.model.Trailer;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TrailerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class TrailerFragment extends Fragment {
+public class TrailerFragment extends Fragment implements View.OnClickListener {
 
     private Trailer mTrailer;
     private TextView mTrailerNameTv;
+    private ImageButton mPlayBtn;
 
     public TrailerFragment() {
         // Required empty public constructor
     }
-
-    /*public static TrailerFragment newInstance(String param1, String param2) {
-        TrailerFragment fragment = new TrailerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }*/
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,7 +51,30 @@ public class TrailerFragment extends Fragment {
     }
 
     private void populateViews() {
-        mTrailerNameTv = getView().findViewById(R.id.trailer_name_tv);
-        mTrailerNameTv.setText(mTrailer.getName());
+        try {
+            mTrailerNameTv = getView().findViewById(R.id.trailer_name_tv);
+            mPlayBtn = getView().findViewById(R.id.play_btn);
+            mTrailerNameTv.setText(mTrailer.getName());
+            mPlayBtn.setOnClickListener(this);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(mTrailer.getType().equals("Youtube"))
+            launchYoutubeVideo();
+    }
+
+    private void launchYoutubeVideo() {
+        Intent appIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + mTrailer.getKey()));
+        Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.youtube.com/watch?v=" + mTrailer.getKey()));
+        try {
+            getActivity().startActivity(appIntent);
+        } catch (ActivityNotFoundException ex) {
+            getActivity().startActivity(webIntent);
+        }
     }
 }
